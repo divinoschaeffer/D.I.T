ac0e53ca0ccbc19a050a5bd83b65557a70876d11
Divino Schaeffer <divino.schaeffer02@gmail.com>
2024-07-13 21:00:13 +0200

    feat:  initialization .dit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Arg, Command};
+use clap::builder::ValueRange;
+use dit::arguments;
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("dit")
+        .version("0.1.0")
+        .author("Schaeffer divino divinoschaeffer@gmail.com")
+        .about("Local version control tool: Divino Information Tracker ")
+        .arg(
+            Arg::new("init")
+                .short('i')
+                .long("init")
+                .num_args(ValueRange::EMPTY)
+                .help("initialize dit repository")
+        )
+        .get_matches();
+    
+    if matches.get_flag("init") {
+        match arguments::init::init_repository(){
+            Ok(()) => (),
+            Err(e) => panic!("Error while initializing dit repository: {e}"),
+        };
+    }
 }
